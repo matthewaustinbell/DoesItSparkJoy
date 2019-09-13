7acaef2cde10f6de783c17e6b5a8d7251d71a052
unknown <matthewaustinbell@gmail.com>
2019-09-13 16:22:25 -0500

    setup

---
+++ DoesItSparkJoy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoesItSparkJoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
